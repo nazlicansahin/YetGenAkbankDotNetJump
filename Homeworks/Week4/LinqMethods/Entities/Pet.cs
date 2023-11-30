ddba0a952e93400b4e1cac69e7b575c9596745d3
Nazlıcan Şahin <76625343+nazlicansahin@users.noreply.github.com>
2023-11-30 19:51:45 +0300

    .

---
+++ Homeworks/Week4/LinqMethods/Entities/Pet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqMethods.Entities
+{
+	internal class Pet
+	{
+
+		
+			public string Name { get; set; }
+			public string Species { get; set; }
+			public int Age { get; set; }
+
+			// Constructor
+			public Pet(string name, string species, int age)
+			{
+				Name = name;
+				Species = species;
+				Age = age;
+			}
+
+			// Method to display pet information
+			public void DisplayInfo()
+			{
+				Console.WriteLine($"Name: {Name}\nSpecies: {Species}\nAge: {Age} years old");
+			}
+		}
+
+	
+}
---
+++ Homeworks/Week4/LinqMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Concat
+using LinqMethods.Entities;
+
+Pet pet1 = new Pet("Buddy", "Dog", 5);
+Pet pet2 = new Pet("Whiskers", "Cat", 3);
+Pet pet3 = new Pet("Charlie", "Dog", 2);
+Pet pet4 = new Pet("Lucy", "Dog", 4);
+Pet pet5 = new Pet("Cooper", "Cat", 1);
+Pet pet6 = new Pet("Daisy", "Cat", 6);
+Pet pet7 = new Pet("Rocky", "Cat", 3);
+Pet pet8 = new Pet("Luna", "Dog", 2);
+
+List<Pet> petList1 = new List<Pet>() { pet1 , pet2, pet3, pet4 };
+List<Pet> petList2 = new List<Pet>() { pet5, pet6 };
+List<Pet> petList3 = new List<Pet>() { pet7, pet8 };
+
+IEnumerable<string> query =
+	petList1.Select(p => p.Name).Concat(petList2.Select(p => p.Name));
+
+foreach (string p in query)
+{
+	Console.WriteLine(p);
+}
+//IEnumerable<string> türü, koleksiyon üzerinde LINQ sorguları çalıştırmak veya döngü işlemleri yapmak gibi çeşitli koleksiyon işlemleri için kullanılabilir.
+
+#endregion
